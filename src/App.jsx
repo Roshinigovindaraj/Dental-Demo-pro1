6b1ec91a068d883ad5a20c5e32c7865ed09e194b
Roshini <devroshini84@gmail.com>
2026-09-25 16:30:28 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,28 @@ import './App.css'
 function App() {
   return (
     <main className="canva-page">
-      <div className="canva-embed-wrapper">
-        <iframe
-          loading="lazy"
-          className="canva-embed"
-          src="https://www.canva.com/design/DAHWMsC3diQ/SbrD8uJWTJabL_WCOpghwQ/view?embed"
-          title="Untitled (1024 x 1536 px) by Roshini"
-          allowFullScreen
-          allow="fullscreen"
-        />
+      <div className="canva-content">
+        <div className="canva-embed-wrapper">
+          <iframe
+            loading="eager"
+            className="canva-embed"
+            src="https://www.canva.com/design/DAHWMsC3diQ/SbrD8uJWTJabL_WCOpghwQ/view?embed"
+            title=""
+            allowFullScreen
+            allow="fullscreen"
+          />
+        </div>
+        <p className="canva-credit">
+          <a
+            href="https://www.canva.com/design/DAHWMsC3diQ/SbrD8uJWTJabL_WCOpghwQ/view?utm_content=DAHWMsC3diQ&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+           
+          </a>
+        
+        </p>
       </div>
-      <p className="canva-credit">
-        <a
-          href="https://www.canva.com/design/DAHWMsC3diQ/SbrD8uJWTJabL_WCOpghwQ/view?utm_content=DAHWMsC3diQ&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Untitled (1024 x 1536 px)
-        </a>{' '}
-        by Roshini
-      </p>
     </main>
   )
 }
